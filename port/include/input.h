0491e90e9d639048d4eba1b358a08220fb419e6b
AL2009man <67606569+AL2009man@users.noreply.github.com>
2025-07-25 19:36:28 -0400

    swapped `JapaneseButtonLayout` order
    to make things a bit more consistent, the order on how to enable or disable has been swapped

--- port/include/input.h
+++ port/include/input.h
@@ -123,8 +123,8 @@ enum mouselockmode {
 
 enum japaneseButtonLayout {
 	JAPANESE_LAYOUT_AUTO = 0, // auto-detect based on controller type
-	JAPANESE_LAYOUT_OFF = 1,   // use standard button layout
-	JAPANESE_LAYOUT_ON = 2     // use Japanese button layout
+	JAPANESE_LAYOUT_ON = 1,   // use japanese button layout
+	JAPANESE_LAYOUT_OFF = 2,  // use standard button layout
 };
 
 // returns bitmask of connected controllers or -1 if failed
