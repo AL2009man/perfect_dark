4f91de458dd0adb84d0b7f0435db52464545035c
AL2009man <67606569+AL2009man@users.noreply.github.com>
2025-08-22 16:34:04 -0400

    updated CrosshairMovement to be more raw

--- src/game/bondmove.c
+++ src/game/bondmove.c
@@ -581,19 +581,22 @@ static void bmoveApplyCrosshairSwivel(struct movedata *movedata, f32 mlookscale,
 /**
  * Apply crosshair movement with scaling and clamping
  */
-static void bmoveApplyCrosshairMovement(f32 aimspeedx, f32 aimspeedy, f32 dx, f32 dy)
+static void bmoveApplyCrosshairAimingMovement(f32 aimspeedx, f32 aimspeedy, f32 dx, f32 dy)
 {
-	const f32 xcoeff = 320.f / 1080.f;
-	const f32 ycoeff = 240.f / 1080.f;
-	const f32 xscale = (aimspeedx * xcoeff) / g_Vars.currentplayer->aspect;
-	const f32 yscale = aimspeedy * ycoeff;
-	f32 x = g_Vars.currentplayer->swivelpos[0] + (dx * xscale);
-	f32 y = g_Vars.currentplayer->swivelpos[1] + (dy * yscale);
-	x = (x < -1.f) ? -1.f : ((x > 1.f) ? 1.f : x);
-	y = (y < -1.f) ? -1.f : ((y > 1.f) ? 1.f : y);
-	g_Vars.currentplayer->swivelpos[0] = x;
-	g_Vars.currentplayer->swivelpos[1] = y;
-	bgunSwivelWithDamp(x, y, 0.01f);
+	// Resolution/Aspect Ratio-based scaling coefficients
+	const f32 xscale = (aimspeedx * 320.f / 1080.f) / g_Vars.currentplayer->aspect;
+	const f32 yscale = aimspeedy * 240.f / 1080.f;
+
+
+	// Calculate new positions with input directly
+	const f32 x = g_Vars.currentplayer->swivelpos[0] + (dx * xscale);
+	const f32 y = g_Vars.currentplayer->swivelpos[1] + (dy * yscale);
+	
+	// Clamping
+	g_Vars.currentplayer->swivelpos[0] = (x < -1.f) ? -1.f : (x > 1.f) ? 1.f : x;
+	g_Vars.currentplayer->swivelpos[1] = (y < -1.f) ? -1.f : (y > 1.f) ? 1.f : y;
+	// Applying to gun swivel system
+	bgunSwivelWithDamp(g_Vars.currentplayer->swivelpos[0], g_Vars.currentplayer->swivelpos[1], 0.01f);
 }
 
 #ifndef PLATFORM_N64
@@ -2538,7 +2541,7 @@ void bmoveProcessInput(bool allowc1x, bool allowc1y, bool allowc1buttons, bool i
         // Gyro is active, apply gyro movement
         inputGyroGetScaledDeltaCrosshair(g_Vars.currentplayernum, &movedata.gyrolookdx, &movedata.gyrolookdy);
         if (movedata.gyrolookdx != 0.0f || movedata.gyrolookdy != 0.0f) {
-            bmoveApplyCrosshairMovement(PLAYER_EXTCFG().gyroaimsensx, PLAYER_EXTCFG().gyroaimsensy, 
+            bmoveApplyCrosshairAimingMovement(PLAYER_EXTCFG().gyroaimsensx, PLAYER_EXTCFG().gyroaimsensy, 
                                         movedata.gyrolookdx, movedata.gyrolookdy);
             return;
         }
@@ -2548,7 +2551,7 @@ void bmoveProcessInput(bool allowc1x, bool allowc1y, bool allowc1buttons, bool i
             f32 dx, dy;
             inputMouseGetScaledDeltaCrosshair(&dx, &dy);
             const f32 norm = g_Vars.lvupdate60freal;
-            bmoveApplyCrosshairMovement(PLAYER_EXTCFG().mouseaimsensx, PLAYER_EXTCFG().mouseaimsensy, dx, dy);
+            bmoveApplyCrosshairAimingMovement(PLAYER_EXTCFG().mouseaimsensx, PLAYER_EXTCFG().mouseaimsensy, dx, dy);
             return;
         }
 #endif
