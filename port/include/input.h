25f95d007e363f8a0963ed6f5f2a4cf4e0329eeb
AL2009man <67606569+AL2009man@users.noreply.github.com>
2025-10-18 19:19:02 -0400

    upgraded Motion Sensor Noise threshold to better handle Accel detection
    Upgraded the Noise Threshold of Motion Sensor, will now be based upon SDL TestController's Gyro/Accel Noise threshold system. However: this requires updating and cleaning up Gyro AutoCalibration system inorder to better support the new threshold

--- port/include/input.h
+++ port/include/input.h
@@ -279,7 +279,6 @@ void inputMouseEnable(s32 enabled);
 
 // Updates the gyro data for the specified controller index
 void inputUpdateGyro(s32 cidx);
-void inputUpdateGyroCalibrationSystem(void);
 
 // Scaled Gyro Movement Retrieval
 void inputGyroGetScaledDelta(s32 cidx, f32* dx, f32* dy, f32* dz);
